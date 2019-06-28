e34a3803509fdb0f1cc92ad26a3da0189618743a
nimeshsilva1997 <nimeshsilva@gmail.com>
2019-06-28 11:37:14 -0400

    Next commit.

---
+++ range.py
@@ -0,0 +1,4 @@
+for x in range(10, 40, 5):  # start from number, up to a number, incrementing by a number
+    print(x)
+for x in range(40, 10, -5):  # start from number, up to a number, decrementing by a number
+    print(x)
