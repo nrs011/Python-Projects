01687a310829aeab81caa126c4bfe345b57dd139
nimeshsilva1997 <nimeshsilva@gmail.com>
2019-06-28 11:08:01 -0400

    Next commit.

---
+++ ifelseif.py
@@ -0,0 +1,10 @@
+name = "Tommy D"
+
+if name is "Bucky":
+    print("Hey there Bucky")
+elif name is "Lucy":
+    print("Hi Lucy")
+elif name is "Sammy":
+    print("Hi Sammy")
+else:
+    print("Please sign up for the site.")
