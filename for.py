6c50efc0fd6fb910af2bcf98f71687f56c92be10
nimeshsilva1997 <nimeshsilva@gmail.com>
2019-06-28 11:20:54 -0400

    Next commit.

---
+++ for.py
@@ -0,0 +1,6 @@
+foods = ['bacon', 'tuna', 'ham', 'sausages', 'beef']  # array of items
+
+# colon with a number means the program will start at the beginning, and ends at a point you want it to end
+for f in foods[:2]:  # the letter f is a variable name
+    print(f)  # indented together means it will be part of the loop
+    print(len(f))  # will print out numbers after strings
