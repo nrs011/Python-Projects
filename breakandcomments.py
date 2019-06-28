ac6c0e8b9edc33e0270d2fbb4c88876b907829a3
nimeshsilva1997 <nimeshsilva@gmail.com>
2019-06-28 12:05:25 -0400

    Next commit.

---
+++ breakandcomments.py
@@ -0,0 +1,18 @@
+magicNumber = 26
+
+# this program finds the variable
+
+print("Age = 21,", "Name = Nimesh R. Silva")
+
+for n in range(101):
+    if n is magicNumber:  # that means we found the variable
+        print(n, "is the magic number.")
+        break  # stops the loop
+    else:
+        print(n)
+
+'''
+for n in range(101):
+    if n is magicNumber:  # that means we found the variable
+        print()
+'''
--- while.py
+++ while.py
@@ -1,1 +1,5 @@
-print("hello")
+test = 5
+
+while test < 10:
+    print(test)
+    test += 1  # adds a number every time
