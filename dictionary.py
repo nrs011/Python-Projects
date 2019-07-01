03cca454625f835f641a45437cb99a8f3746c988
nimeshsilva1997 <nimeshsilva@gmail.com>
2019-07-01 11:45:06 -0400

    Next commit.

---
+++ dictionary.py
@@ -0,0 +1,7 @@
+classmates = {'Tony': ' cool but smells', 'Emma': ' sits behind me', 'Lucy': ' asks too many questions'}
+
+print(classmates)
+print(classmates['Emma'])
+
+for k, v in classmates.items():  # whenever we loop through it, k is the name, and v is the value
+    print(k + v)
