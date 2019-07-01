5096d74f5f8eb462e2073c1680e74689d8341871
nimeshsilva1997 <nimeshsilva@gmail.com>
2019-07-01 10:47:23 -0400

    Next commit.

---
+++ keywordargument.py
@@ -0,0 +1,7 @@
+def dumb_sentence(name='Nimesh', action='ate', item='tuna'):
+    print(name, action, item)
+
+dumb_sentence()
+dumb_sentence("Sally", "farts", "gently")
+dumb_sentence(item='awesome')  # names something different
+dumb_sentence(item='awesome', action='is')
---
+++ variablescope.py
@@ -0,0 +1,9 @@
+def corn():
+    a = 7823
+    print(a)
+
+def fudge():
+    print(a)
+
+corn()
+fudge()
